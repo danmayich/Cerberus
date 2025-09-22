3296ef500cb47574fdaeb1f91472b3c062062056
Dan <danielmayich@yahoo.com>
2025-09-21 20:47:40 -0400

    Working vue launch

--- Cerberus.AppHost/Program.cs
+++ Cerberus.AppHost/Program.cs
@@ -1,5 +1,15 @@
+using k8s.Models;
+
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Cerberus_Api>("cerberus");
+var api = builder.AddProject<Projects.Cerberus_Api>("cerberus");
+
+builder.AddNpmApp(name: "vue", workingDirectory: "../Cerberus.UI")
+.WithReference(api)
+.WaitFor(api)
+.WithHttpEndpoint(env: "PORT")
+.WithExternalHttpEndpoints();
+
 
 builder.Build().Run();
